8b1404cf1d5600c0bb029e83d76031dfe7a34af5
Yash Surjuse <yashrsurjused@gmail.com>
2026-01-25 18:34:38 +0530

    Create Check Triangle Type.py
    Add a program to check the type of triangle based on side lengths.

---
+++ Conditional Statement/Check Triangle Type.py
@@ -0,0 +1,17 @@
+a = int(input("Enter first side: "))
+b = int(input("Enter second side: "))
+c = int(input("Enter third side: "))
+
+if a == b == c:
+    print("Equilateral")
+elif a == b or b == c or a == c:
+    print("Isosceles")
+else:
+    print("Scalene")
+
+#output:
+Enter first side: 5
+Enter second side: 5
+Enter third side: 5
+
+Equilateral
