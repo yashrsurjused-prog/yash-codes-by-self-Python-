ec40c0cf83f68f80c9f9364ea1167f62aa855973
Yash Surjuse <yashrsurjused@gmail.com>
2026-01-26 14:26:41 +0530

    Create reverse_table.py

---
+++ Loops/reverse_table.py
@@ -0,0 +1,17 @@
+n = int(input("Enter a number: "))
+
+for i in range(10, 0, -1):
+    print(n, "x", i, "=", n * i)
+
+#output:
+5
+5 x 10 = 50
+5 x 9 = 45
+5 x 8 = 40
+5 x 7 = 35
+5 x 6 = 30
+5 x 5 = 25
+5 x 4 = 20
+5 x 3 = 15
+5 x 2 = 10
+5 x 1 = 5
